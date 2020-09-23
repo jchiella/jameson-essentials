a79fe1d7bde871f4f219b98dd366b7d666433c84
James Chiella <james.chiella@gmail.com>
2020-09-23 12:46:56 -0400

    fixing sayMention

--- utils.js
+++ utils.js
@@ -2,9 +2,7 @@ const sayMention = (client, channel, person, text) => {
     const personID = person.slice(3, -1);
     const botID = getUserIDFromName(client, 'JamesonEssentials');
 
-    if (person === 'me') {
-      channel.send(text);
-    } else if (personID === botID) {
+    if (person === 'me' || personID === botID) {
       message.channel.send(`${message.author} ${text}`);
     } else {
       channel.send(`${person} ${text}`);
