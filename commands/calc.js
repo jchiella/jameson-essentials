d67171e37c0e472317261b23431de2940ec85e2c
James Chiella <james.chiella@gmail.com>
2020-09-22 21:49:06 -0400

    fixing calc 2

--- commands/calc.js
+++ commands/calc.js
@@ -4,15 +4,18 @@ const apiURL = 'http://api.mathjs.org/v4/';
 
 module.exports = (message, args) => {
   if (args.length > 0) {
+    let math = args.join(' ');
+    console.log('math: ', math);
     axios.get(apiURL, {
       params: {
-        expr: args.join(' '),
+        expr: math,
       },
     })
       .then(function (response) {
         message.channel.send(reponse.data);
       })
       .catch(function (error) {
+        console.log(error);
         message.channel.send('Nope! That was not good math!');
       });
   }
