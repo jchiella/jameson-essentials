0b935d3844a88e620232779b7a33fc1149da6262
James Chiella <james.chiella@gmail.com>
2021-02-04 16:54:22 -0500

    db integration

--- commands/count.js
+++ commands/count.js
@@ -1,34 +1,70 @@
-const counters = {};
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+});
+
+const init = () => {
+  client.connect();
+};
+
+const newCounter = (counter) => {
+  await client.query(`INSERT INTO Counters (Name, Count) VALUES ('${counter}', 0);`);
+};
+
+const getCounter = (counter) => {
+  const result = await client.query(`SELECT Count FROM Counters WHERE Name='${counter}'`);
+  console.log(result);
+  return 101;
+};
+
+const incrementCounter = (counter) => {
+  await client.query(`UPDATE Counters SET Count = Count + 1 WHERE Name='${counter}';`);
+};
+
+const decrementCounter = (counter) => {
+  await client.query(`UPDATE Counters SET Count = Count + 1 WHERE Name='${counter}';`);
+};
+
+const setCounter = (counter, number) => {
+  await client.query(`UPDATE Counters SET Count = ${number} WHERE Name='${counter}';`);
+};
 
 const handler = ({ channel }, args) => {
-  if (args.length >= 2) {
+  if (args.length === 1) {
+    const counter = args.shift();
+    channel.send(`The ${counter} counter is ${getCounter(counter)}`);
+  } else if (args.length >= 2) {
     const counter = args.shift();
     const subcommand = args.shift();
     if (subcommand === 'new') {
-      counters[counter] = 0;
+      newCounter(counter);
       channel.send(`Created new counter ${counter}`);
     } else if (subcommand === '+') {
-      counters[counter] += 1;
-      channel.send(`${counter} is now ${counters[counter]}`);
+      incrementCounter(counter);
+      channel.send(`${counter} is now ${getCounter(counter)}`);
     } else if (subcommand === '-') {
-      counters[counter] -= 1;
-      channel.send(`${counter} is now ${counters[counter]}`);
+      decrementCounter(counter);
+      channel.send(`${counter} is now ${getCounter(counter)}`);
     } else if (subcommand === '=') {
       if (args.length) {
         let number;
         try {
           number = args.shift();
         } finally {
-          counters[counter] = number;
-          channel.send(`${counter} is now ${counters[counter]}`);
+          setCounter(counter, number);
+          channel.send(`${counter} is now ${getCounter(counter)}`);
         }
       }
     }
   }
-}
+};
 
 module.exports = {
   handler,
   name: 'count',
-  init: () => 0,
+  init,
 }
