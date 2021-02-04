966a295f8b6212a09da6c0bd5886677c90269efd
James Chiella <james.chiella@gmail.com>
2021-02-04 16:58:30 -0500

    awaits

--- commands/count.js
+++ commands/count.js
@@ -11,52 +11,52 @@ const init = () => {
   client.connect();
 };
 
-const newCounter = (counter) => {
+const newCounter = async (counter) => {
   await client.query(`INSERT INTO Counters (Name, Count) VALUES ('${counter}', 0);`);
 };
 
-const getCounter = (counter) => {
+const getCounter = async (counter) => {
   const result = await client.query(`SELECT Count FROM Counters WHERE Name='${counter}'`);
   console.log(result);
   return 101;
 };
 
-const incrementCounter = (counter) => {
+const incrementCounter = async (counter) => {
   await client.query(`UPDATE Counters SET Count = Count + 1 WHERE Name='${counter}';`);
 };
 
-const decrementCounter = (counter) => {
+const decrementCounter = async (counter) => {
   await client.query(`UPDATE Counters SET Count = Count + 1 WHERE Name='${counter}';`);
 };
 
-const setCounter = (counter, number) => {
+const setCounter = async (counter, number) => {
   await client.query(`UPDATE Counters SET Count = ${number} WHERE Name='${counter}';`);
 };
 
-const handler = ({ channel }, args) => {
+const handler = async ({ channel }, args) => {
   if (args.length === 1) {
     const counter = args.shift();
-    channel.send(`The ${counter} counter is ${getCounter(counter)}`);
+    channel.send(`The ${counter} counter is ${await getCounter(counter)}`);
   } else if (args.length >= 2) {
     const counter = args.shift();
     const subcommand = args.shift();
     if (subcommand === 'new') {
-      newCounter(counter);
+      await newCounter(counter);
       channel.send(`Created new counter ${counter}`);
     } else if (subcommand === '+') {
-      incrementCounter(counter);
-      channel.send(`${counter} is now ${getCounter(counter)}`);
+      await incrementCounter(counter);
+      channel.send(`${counter} is now ${await getCounter(counter)}`);
     } else if (subcommand === '-') {
-      decrementCounter(counter);
-      channel.send(`${counter} is now ${getCounter(counter)}`);
+      await decrementCounter(counter);
+      channel.send(`${counter} is now ${await getCounter(counter)}`);
     } else if (subcommand === '=') {
       if (args.length) {
         let number;
         try {
           number = args.shift();
         } finally {
-          setCounter(counter, number);
-          channel.send(`${counter} is now ${getCounter(counter)}`);
+          await setCounter(counter, number);
+          channel.send(`${counter} is now ${await getCounter(counter)}`);
         }
       }
     }
