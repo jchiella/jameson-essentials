20011def0f569a2fef7887a853118497d85008a6
James Chiella <james.chiella@gmail.com>
2021-04-03 19:17:54 -0400

    fixing mongo env variable

--- globals.js
+++ globals.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const botToken = process.env.BOT_TOKEN;
 const port = process.env.PORT || 3000;
 const commandPrefix = '$';
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.MONGO_URL;
 
 module.exports = {
   botToken,
